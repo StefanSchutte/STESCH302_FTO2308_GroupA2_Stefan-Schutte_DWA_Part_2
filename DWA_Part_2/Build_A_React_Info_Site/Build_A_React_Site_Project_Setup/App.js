b9016d799783afe0023135a49f96124b20163386
Stefan Schutte <2011070287@ufs4life.ac.za>
2024-01-24 23:27:21 +0200

    info_site_3

---
+++ DWA_Part_2/Build_A_React_Info_Site/Build_A_React_Site_Project_Setup/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Main from './components/Main.js'
+import Navbar from "./components/Navbar.js";
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
