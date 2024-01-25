173a23df1a4ebb4e85b574d0bb2056470b8de1bd
Stefan Schutte <2011070287@ufs4life.ac.za>
2024-01-25 21:54:07 +0200

    info_comp

--- DWA_Part_2/Build_A_React_Info_Site/Build_A_React_Site_Project_Setup/components/Navbar.js
+++ DWA_Part_2/Build_A_React_Info_Site/Build_A_React_Site_Project_Setup/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Navbar() {
     return (
         <nav>
-            <img src='../images/img_1.png.png' alt='react-logo' className="nav--icon"/>
+            <img src='../images/img_1.png' alt='react-logo' className="nav--icon"/>
                 <h3 className="nav--logo_text">ReactFacts</h3>
                 <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
