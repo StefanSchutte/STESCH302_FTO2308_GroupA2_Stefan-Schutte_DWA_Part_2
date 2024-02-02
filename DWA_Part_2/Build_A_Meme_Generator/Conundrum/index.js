9154ed895f280c60e71931cd90479e7244f527b0
Stefan Schutte <2011070287@ufs4life.ac.za>
2024-02-02 13:24:37 +0200

    meme3

--- DWA_Part_2/Build_A_Meme_Generator/Conundrum/index.js
+++ DWA_Part_2/Build_A_Meme_Generator/Conundrum/index.js
@@ -1,15 +1,40 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+//
+// function App() {
+//     const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
+//
+//     const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
+//
+//     function addItem(){
+//         const newThingText = `Thing ${thingsArray.length +1}`
+//         // thingsArray.push(newThingText)
+//         setThings(prevState => [...prevState, newThingText])
+//     }
+//
+//     return (
+//         <div>
+//             <button onClick={addItem}>Add Item</button>
+//             {thingsElements}
+//         </div>
+//     )
+// }
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+//Complex State: Arrays
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-    const [things, setThings] = React.useState(["Thing 1", "Thing 2"])
+    const [thingsArray, setThingsArray] = React.useState(["Thing 1", "Thing 2"])
 
-    const thingsElements = things.map(thing => <p key={thing}>{thing}</p>)
+
+    const thingsElements = thingsArray.map(thing => <p key={thing}>{thing}</p>)
 
     function addItem(){
-        const newThingText = `Thing ${thingsArray.length +1}`
-        // thingsArray.push(newThingText)
-        setThings(prevState => [...prevState, newThingText])
+        setThingsArray(prevThingArray => [...prevThingArray, `Thing ${prevThingArray.length + 1}`])
     }
 
     return (
