d39e3fe8689f644e010c9e3711f881fc907167b4
Stefan Schutte <2011070287@ufs4life.ac.za>
2024-02-20 09:26:40 +0200

    22.2

--- DWA_Part_2/DWA_22_Storybook/DWA_22_1_Using_Storybook/movies-api-example-main/movie-app/src/components/Filters/Filters.tsx
+++ DWA_Part_2/DWA_22_Storybook/DWA_22_1_Using_Storybook/movies-api-example-main/movie-app/src/components/Filters/Filters.tsx
@@ -5,45 +5,110 @@ import {
   DialogContent,
   DialogTitle,
   TextField,
+  Alert,
 } from "@mui/material";
-
-import { FormEvent } from "react";
+import { useSpring } from "@react-spring/web";
+import { FormEvent, useState, useRef, useEffect } from "react";
 import z from "zod";
+import styled from "@emotion/styled";
 
 const response = z.object({
-  search: z.string(),
+  search: z
+    .string()
+    .min(3, {
+      message: "Search needs to be empty or atleast 3 charactes.",
+    })
+    .optional()
+    .or(z.literal("")),
 });
 
-type Response = z.infer<typeof response>
+type Response = z.infer<typeof response>;
 
 export type Filters = {
   onSubmit: (response: Response) => void;
 };
 
-export const Filters = (props: Filters) => {
+const StyledAlert = styled(Alert)<{ error: string | null }>`
+  margin-top: 1rem;
+  height: ${(props) => (props.error ? auto : "0.1rem")};
+`;
+
+const useFilters = (props: Filters) => {
+  const [error, setError] = useState<string | null>(null);
+
+  const isStarting = useRef(true);
+
+  const style = useSpring({
+    from: { opacity: isStarting || error ? 0 : 1 },
+    to: { opacity: error ? 1 : 0 },
+  });
+
+  useEffect(() => {
+    isStarting.current = false;
+  }, []);
+
   const { onSubmit } = props;
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    };
+    const entries = new FormData(event.target as any);
+    const transformed = Object.fromEntries(entries);
+    const validation = response.safeParse(transformed);
+
+    if (!validation.success) {
+      const { error } = validation;
+      return setError(error.issues[0].message);
+      console.log(error.issues[0].message);
+    }
+
+    setError(null);
+    const { data } = validation;
+    onSubmit(data);
   };
+  return {
+    error,
+    style,
+    handleSubmit,
+  };
+};
+
+const Content = styled.div`
+  width: 100%;
+  width: 20rem;
+  max-width: 20rem;
+`;
+
+export const Filters = (props: Filters) => {
+  const { error, handleSubmit, style } = useFilters(props);
 
   return (
     <Dialog open>
-      <DialogTitle>Filters</DialogTitle>
-      <DialogContent>
-        <form id="filters" onSubmit={handleSubmit}>
-          <TextField label="search" variant="filled" />
-        </form>
-      </DialogContent>
+      <Content>
+        <DialogTitle>Filters</DialogTitle>
+        <DialogContent>
+          <form id="filters" onSubmit={handleSubmit}>
+            <TextField
+              fullWidth
+              name="Search"
+              label="search"
+              variant="filled"
+              placeholder="Any"
+            />
+          </form>
 
-      <DialogActions>
-        <Button variant="outlined">Cancel</Button>
-        <Button variant="contained" type="submit" form="filters">
-          Apply
-        </Button>
-      </DialogActions>
+          <animated.div style={style}>
+            <StyledAlert error={error} severity="warning"></StyledAlert>
+          </animated.div>
+        </DialogContent>
+
+        <DialogActions>
+          <Button variant="outlined">Cancel</Button>
+          <Button variant="contained" type="submit" form="filters">
+            Apply
+          </Button>
+        </DialogActions>
+      </Content>
     </Dialog>
   );
 };
