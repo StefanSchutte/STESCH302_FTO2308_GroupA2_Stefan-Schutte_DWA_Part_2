7e0b63dcf30056d4f1b93c21711874b4f0e9fe12
Stefan Schutte <2011070287@ufs4life.ac.za>
2024-02-29 10:12:16 +0200

    23

--- DWA_Part_2/Build_A_React_Info_Site/Build_A_React_Site_Part_2/index.js
+++ DWA_Part_2/Build_A_React_Info_Site/Build_A_React_Site_Part_2/index.js
@@ -1,24 +1,9 @@
-/**
- Challenge:
+import React from "react";
+import ReactDOM from "react-dom";
 
- Part 2:
- - Add a `header` element with a nested `nav` element. Inside the `nav`,
- include a `img` element with the image of the React logo inside
- (src="./react-logo.png") and make sure to set the width to something
- more manageable so it doesn't take up the whole screen
- - Add an `h1` with some text describing the page. (E.g. "Reasons
- I'm excited to learn React"). Place it above the ordered list.
- - Add a `footer` after the list that says:
- "© 20xx <last name here> development. All rights reserved."
-
- */
-
-import React from "react"
-import ReactDOM from "react-dom"
-
-import Header from "./Header.js"
-import MainContent from "./MainContent.js"
-import Footer from "./Footer.js"
+import Header from "./Header.js";
+import MainContent from "./MainContent.js";
+import Footer from "./Footer.js";
 //children component
 // function Header() {
 //     return (
@@ -50,13 +35,13 @@ import Footer from "./Footer.js"
 // }
 //parent component
 function Page() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
 }
 
 // function Footer() {
@@ -67,7 +52,7 @@ function Page() {
 //     )
 // }
 
-ReactDOM.render(<Page/>, document.getElementById("root"))
+ReactDOM.render(<Page />, document.getElementById("root"));
 
 /*
 Quiz!
